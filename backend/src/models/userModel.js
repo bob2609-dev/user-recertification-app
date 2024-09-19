69799d14c34bc91f0181569a15931f06088a76a3
Mwaimu Mtingele <wizzbx@gmail.com>
2024-09-19 23:10:53 +0300

    create users table and insert details

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -3,6 +3,7 @@ const dbConfig = require("../config/dbConfig");
 
 const db = mysql.createConnection(dbConfig);
 
+// Connect to MySQL
 db.connect((err) => {
   if (err) {
     console.error("Error connecting to MySQL:", err.message);
@@ -11,22 +12,21 @@ db.connect((err) => {
   console.log("Connected to MySQL database!");
 });
 
+// Create users table if it does not exist
 const createUserTable = `
- CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  ad_username VARCHAR(255) NOT NULL UNIQUE,
-  email VARCHAR(255) UNIQUE,
-  first_name VARCHAR(255),
-  last_name VARCHAR(255),
-  active BOOLEAN DEFAULT true,
-  role VARCHAR(50),
-  last_login TIMESTAMP,
-  is_deleted BOOLEAN DEFAULT false,
-  -- permissions JSON,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
+  CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ad_username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) UNIQUE,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    active BOOLEAN DEFAULT true,
+    role VARCHAR(50),
+    last_login TIMESTAMP,
+    is_deleted BOOLEAN DEFAULT false,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  );
 `;
 
 db.query(createUserTable, (err) => {
@@ -35,4 +35,5 @@ db.query(createUserTable, (err) => {
   }
 });
 
+// Export the database connection and model functions
 module.exports = db;
--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -2,17 +2,16 @@ const db = require('../models/userModel');
 
 // Create a new user
 const createUser = (user) => {
-    return new Promise((resolve, reject) => {
-      const query = 'INSERT INTO users SET ?';
-      db.query(query, user, (err, results) => {
-        if (err) {
-          console.error('Error inserting user:', err.message);
-          return reject(err);
-        }
-        resolve(results);
-      });
+  return new Promise((resolve, reject) => {
+    const query = 'INSERT INTO users SET ?';
+    db.query(query, user, (err, results) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(results);
     });
-  };
+  });
+};
 
 // Get all users
 const getAllUsers = () => {
